9840bad912f7eedc7f495c251d761625ac39a39b
Kieran <itsjusttriz@gmail.com>
2025-08-18 03:59:40 +0100

    re-handle new class-shaped events

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -1,28 +1,37 @@
-import { Client, Collection, GatewayIntentBits } from 'discord.js';
-import type { CommandData } from './types';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+
+import { DiscordReadyEvent } from './events/onReady';
+import { DiscordInteractionCreateEvent } from './events/onInteractionCreate';
+
+import type { MCPackTracker } from '..';
+import type { CommandCollection, CommandData } from './types';
 
 // TODO: Remove the old imports.
-import * as Handlers from './handlers';
 import Env from '../env';
 
 export class DiscordBot {
-	private _commands = new Collection<string, CommandData>();
+	private _commands: CommandCollection = new Collection<
+		string,
+		CommandData
+	>();
+
 	private _client = new Client({
 		intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 	});
 
+	constructor(private _app: MCPackTracker) {}
+
 	// TODO: Maybe remove 'async'?
 	async login() {
-		// TODO: Change this to a class.
-		await Handlers.onReady({
-			client: this._client,
-			commands: this._commands,
-		});
-		// TODO: Change this to a class.
-		await Handlers.onInteractionCreate({
-			client: this._client,
-			commands: this._commands,
-		});
+		this._client.on(
+			Events.ClientReady,
+			(c) => new DiscordReadyEvent(c, this._commands)
+		);
+
+		this._client.on(
+			Events.InteractionCreate,
+			(i) => new DiscordInteractionCreateEvent(this._commands, i)
+		);
 
 		await this._client.login(Env.DISCORD_TOKEN);
 	}
