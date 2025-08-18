7addb7e987e9c9f5ccb60103b8d565e6de09eab2
Kieran <itsjusttriz@gmail.com>
2025-08-18 03:59:55 +0100

    re-shape types

--- src/discord/types.ts
+++ src/discord/types.ts
@@ -5,12 +5,16 @@ import type {
 	SlashCommandBuilder,
 } from 'discord.js';
 
+export type DiscordReadyClient = Client<true>;
+
 export type CommandData = {
 	metadata: ReturnType<SlashCommandBuilder['toJSON']>;
 	handle: (i: ChatInputCommandInteraction) => void;
 };
 
+export type CommandCollection = Collection<string, CommandData>;
+
 export type HandlerContext = {
 	client: Client;
-	commands: Collection<string, CommandData>;
+	commands: CommandCollection;
 };
