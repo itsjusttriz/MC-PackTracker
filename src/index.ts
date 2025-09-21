bc98c43da58487188ba8970cdd747a57a2a1e45d
Kieran <itsjusttriz@gmail.com>
2025-09-21 19:37:46 +0100

    add temp timestamps to logging

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { DiscordBot } from './discord';
 
+//TODO: Refactor this.
+const oldLog = console.log;
+console.log = function (...args) {
+	oldLog(new Date().toISOString(), '|', ...args);
+};
+
 export class MCPackTracker {
 	static async start() {
 		const discordBot = DiscordBot.getInstance();
